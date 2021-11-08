9258d1296d51a2bb74b94f9ba5a1cc247ce456cd
e <3304727652@qq.com>
2021-11-08 10:37:24 +0800

    初始化配置

---
+++ Service/src/main/java/com/squash/rpc/MainApplication.java
@@ -0,0 +1,17 @@
+package com.squash.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @Author Squash
+ * @Data 2021/11/8 10:29
+ */
+@EnableAspectJAutoProxy(exposeProxy = true)
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
